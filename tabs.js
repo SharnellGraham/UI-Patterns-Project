3c617bd164e5c8aafa1abdcc1ad892858a3afa33
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2022-12-31 01:16:26 -0500

    final changes

--- tabs.js
+++ tabs.js
@@ -5,3 +5,19 @@ tabBtn.forEach(function(element) {
    element.addEventListener("click", openTabs);
 });
 
+function openTabs(evt) {
+    const btnTarget = evt.currentTarget;
+    const item = btnTarget.dataset.item;
+  
+     tabContents.forEach(function(item) {
+        item.classList.remove("tab__item--active");
+     });
+  
+     tabBtn.forEach(function(item) {
+        item.classList.remove("tab__btn--active");
+     });
+  
+     document.querySelector(`#${item}`).classList.add("tab__item--active");
+  
+     btnTarget.classList.add("tab__btn--active"); 
+  }
